575704a3d1161dbf756415f3151d3a175f5833bb
devbrian <34463288+devbrian@users.noreply.github.com>
2021-01-26 16:49:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,25 @@ provider "google" {
  project     = "future-abacus-302701"
  region      = "us-east1"
 }
+
+resource "google_compute_instance" "example" {
+  name          = "example"
+  machine_type  = "f1-micro"
+  zone          = "us-east1-b"
+  
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
+    }
+  }
+  
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  
+  tags = ["terraform-example"]
+}
