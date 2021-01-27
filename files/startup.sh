f7be927073495904d5d0de4680ff1b9a97e2f678
devbrian <34463288+devbrian@users.noreply.github.com>
2021-01-26 19:50:29 -0600

    Update startup.sh

--- files/startup.sh
+++ files/startup.sh
@@ -4,7 +4,6 @@ wget https://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run
 rm nzbget-latest-bin-linux.run
 
-envsubst < /tmp/nzbget.conf
 rm /opt/nzbget/nzbget.conf
 mv /tmp/nzbget.conf /opt/nzbget/nzbget.conf
 chmod 777 /opt/nzbget/nzbget.conf
