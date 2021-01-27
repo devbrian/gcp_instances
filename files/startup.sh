f76907aedc9aafdd8a06f8a897caf069a360a569
devbrian <34463288+devbrian@users.noreply.github.com>
2021-01-26 21:16:19 -0600

    Update startup.sh

--- files/startup.sh
+++ files/startup.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 cd /opt/
+
 wget https://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run
 rm nzbget-latest-bin-linux.run
-
 rm /opt/nzbget/nzbget.conf
 mv /tmp/nzbget.conf /opt/nzbget/nzbget.conf
 chmod 777 /opt/nzbget/nzbget.conf
@@ -12,5 +12,15 @@ curl https://rclone.org/install.sh | sudo bash
 mkdir --parents /root/.config/rclone/
 mv /tmp/rclone.conf /root/.config/rclone/rclone.conf
 
+git clone https://github.com/l3uddz/cloudplow /opt/cloudplow
+apt install python3-pip
+cd /opt/cloudplow
+python3 -m pip install -r requirements.txt
+ln -s /opt/cloudplow/cloudplow.py /usr/local/bin/cloudplow
+mv /tmp/config.json /opt/cloudplow/config.json
+
 systemctl enable nzbget.service
 systemctl start nzbget.service
+
+systemctl enable cloudplow.service
+systemctl start cloudplow.service
