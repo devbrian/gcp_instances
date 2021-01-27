5987e34b55e273ac65c43c5bbe5367f5969faa01
devbrian <34463288+devbrian@users.noreply.github.com>
2021-01-26 20:39:42 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "google_compute_instance" "instance1" {
   
    provisioner "file" {
         content = var.rclone_file
-        destination = "/root/.config/rclone/rclone.conf"
+        destination = "/tmp/rclone.conf"
         connection {
             type = "ssh"
             host = google_compute_instance.instance1.network_interface.0.access_config.0.nat_ip
