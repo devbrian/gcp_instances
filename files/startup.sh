92e64f9ba9e160b78ef91bf4ee8b2e4b0513cff1
devbrian <34463288+devbrian@users.noreply.github.com>
2021-01-26 18:39:57 -0600

    Update startup.sh

--- files/startup.sh
+++ files/startup.sh
@@ -3,3 +3,12 @@ cd /opt/
 wget https://nzbget.net/download/nzbget-latest-bin-linux.run
 sh nzbget-latest-bin-linux.run
 rm nzbget-latest-bin-linux.run
+
+sed -i "s/^\(Server1\.Username\s*=\s*\).*\$/\1$s1user/" /tmp/nzbget.conf
+sed -i "s/^\(Server1\.Password\s*=\s*\).*\$/\1$s1pass/" /tmp/nzbget.conf
+sed -i "s/^\(ControlUsername\s*=\s*\).*\$/\1$user/" /tmp/nzbget.conf
+sed -i "s/^\(ControlPassword\s*=\s*\).*\$/\1$pass/" /tmp/nzbget.conf
+
+rm /opt/nzbget/nzbget.conf
+mv /tmp/nzbget.conf /opt/nzbget/nzbget.conf
+chmod 777 /opt/nzbget/nzbget.conf
