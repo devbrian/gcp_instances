c3f0f2fddb7603c84ac16505ad51560a2fea3e3f
devbrian <34463288+devbrian@users.noreply.github.com>
2021-05-15 11:55:34 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "template_file" "rclone_template" {
 
 provider "google" {
  credentials = var.google_sa
- project     = "pelagic-bastion-313721"
+ project     = var.google_project
  region      = "us-east1"
 }
 
